273be11cb8dd7b52976cc44294cc0f9b80871828
Eleonora Mattera <m.eleonora98@live.it>
2023-07-04 22:27:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import CustomNav from "./components/CustomNav";
+
+function App() {
+  return (
+    <div className="App">
+      <CustomNav />
+    </div>
+  );
+}
+
+export default App;
