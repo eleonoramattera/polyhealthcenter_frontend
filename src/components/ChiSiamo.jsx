653867deb0af1ccce3f9780598c901269e1c0ad0
Eleonora Mattera <m.eleonora98@live.it>
2023-07-05 16:23:15 +0200

    Chi siamo (placeholder)

---
+++ src/components/ChiSiamo.jsx
@@ -0,0 +1,22 @@
+import { Row, Col } from "react-bootstrap";
+
+function ChiSiamo() {
+  return (
+    <Row>
+      <Col xs={12} className="mt-5">
+        <h2>CHI SIAMO</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur aspernatur ab error veniam quos natus
+          magni officia mollitia molestias dolores aperiam rem distinctio quo, magnam quidem suscipit. Blanditiis, ipsa
+          excepturi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur aspernatur ab error veniam
+          quos natus magni officia mollitia molestias dolores aperiam rem distinctio quo, magnam quidem suscipit.
+          Blanditiis, ipsa excepturi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur aspernatur
+          ab error veniam quos natus magni officia mollitia molestias dolores aperiam rem distinctio quo, magnam quidem
+          suscipit. Blanditiis, ipsa excepturi.
+        </p>
+      </Col>
+    </Row>
+  );
+}
+
+export default ChiSiamo;
