aef1ccf98b4835844ef75678d505dbbca1ab6b1b
Eleonora Mattera <m.eleonora98@live.it>
2023-07-05 16:23:58 +0200

    sedi - Dove siamo

---
+++ src/components/Map.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Map = () => {
+  return (
+    <div className="my-5">
+      <h2> DOVE SIAMO</h2>
+      <iframe
+        src="https://www.google.com/maps/d/embed?mid=12S4AB3AFFEVVC8dc6jtDVnZ8UozJrmI&ehbc=2E312F"
+        width="640"
+        height="480"></iframe>
+    </div>
+  );
+};
+
+export default Map;
