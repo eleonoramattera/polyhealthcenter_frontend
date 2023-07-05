177353108c9a5a82c972c492df1b111d3ca36c79
Eleonora Mattera <m.eleonora98@live.it>
2023-07-05 16:22:43 +0200

    css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import CustomNav from "./components/CustomNav";
+import Footer from "./components/Footer";
+import ChiSiamo from "./components/ChiSiamo";
+import Map from "./components/Map.jsx";
 
 function App() {
   return (
     <div className="App">
       <CustomNav />
+      <ChiSiamo />
+      <Map />
+      <Footer />
     </div>
   );
 }
