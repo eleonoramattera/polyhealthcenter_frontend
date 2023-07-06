6e9e8177914f72dcf9a314e008954f62305e4df1
Eleonora Mattera <m.eleonora98@live.it>
2023-07-06 20:02:52 +0200

    home

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import Map from "./Map.jsx";
+import VideoPlayer from "./VideoPlayer.jsx";
+import ChiSiamo from "./ChiSiamo.jsx";
+
+function Home() {
+  return (
+    <div>
+      <VideoPlayer />
+      <ChiSiamo />
+      <Map />
+    </div>
+  );
+}
+
+export default Home;
