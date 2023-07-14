e476af28a8e77408aa1fc5d54a4706ed05a18dd7
Eleonora Mattera <m.eleonora98@live.it>
2023-07-14 23:43:40 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Row, Col } from "react-bootstrap";
 
 function Footer() {
   return (
-    <footer>
+    <footer className="mt-5">
       <Row className="text-center mt-5 text-white fixed-bottom" style={{ backgroundColor: "#295651" }}>
         <Col xs={12}>
           <div>Copyright PolyHealthCenter - Tutti i diritti riservati - P.IVA 1234567890</div>
