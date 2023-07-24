81274ff957c15b233b6fff55935a70c3131ec2b4
Eleonora Mattera <m.eleonora98@live.it>
2023-07-25 00:02:50 +0200

    fix components

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ import Registrazione from "./components/Registrazione.jsx";
 import Login from "./components/Login.jsx";
 import Home from "./components/Home.jsx";
 import ChiSiamo from "./components/ChiSiamo";
-import Terapie from "./components/Terapie";
-import Prenotazioni from "./components/Prenotazioni";
 
+import Prenotazioni from "./components/Prenotazioni";
+import TerapiaStore from "./components/TerapiaStore";
 import DoveSiamo from "./components/DoveSiamo";
+
+import RegisterPage from "./components/RegisterPage";
+import LeMiePrenotazioni from "./components/LeMiePrenotazioni";
+import Pren from "./components/Pren";
 
 function App() {
   return (
@@ -19,14 +23,14 @@ function App() {
         <CustomNav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="prenotazioni" element={<Prenotazioni />} />
-          <Route path="/registrazione" element={<Registrazione />} />
+          <Route path="prenotazioni" element={<Pren />} />
+          <Route path="/registerpage" element={<RegisterPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/chisiamo" element={<ChiSiamo />} />
-          <Route path="/terapie" element={<Terapie />} />
+          <Route path="/terapiastore" element={<TerapiaStore />} />
           <Route path="/dovesiamo" element={<DoveSiamo />} />
+          <Route path="/lemieprenotazioni" element={<LeMiePrenotazioni />} />
         </Routes>
-
         <Footer />
       </BrowserRouter>
     </div>
