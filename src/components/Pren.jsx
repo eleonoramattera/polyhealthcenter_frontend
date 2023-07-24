cfde57834f0a567f04d91021f4c9544f3e9fb96a
Eleonora Mattera <m.eleonora98@live.it>
2023-07-25 00:03:42 +0200

    fix Pren

--- src/components/Pren.jsx
+++ src/components/Pren.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import { Col, Container, Row, Form, Button } from "react-bootstrap";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import { Link, useLocation } from "react-router-dom";
 
 function Pren() {
+  const location = useLocation();
+  console.log("LOCATION", location);
+
   const [id, setId] = useState(0);
-  const [nomeTerapia, setNomeTerapia] = useState("");
+  const [nomeTerapia, setNomeTerapia] = useState([]);
   const [dataPrenotazione, setDataPrenotazione] = useState("");
   const [sede, setSede] = useState("");
   const [nomeUtente, setNomeUtente] = useState("");
@@ -21,40 +23,6 @@ function Pren() {
     return `${year}-${month}-${day}T${hours}:${minutes}`;
   }
 
-  {
-    /*  // Funzione per gestire il cambiamento della data e dell'ora
-  function handleDateTimeChange(e) {
-    const selectedDateTime = e.target.value;
-    if (isDateTimeValid(selectedDateTime)) {
-      setDataPrenotazione(selectedDateTime);
-    } else {
-      // Puoi gestire l'errore in qualche modo, come mostrando un messaggio all'utente
-      alert("L'ora selezionata non è valida. Siamo aperti dalle 8 alle 19");
-    }
-  }
-
-  function handleDateChange(e) {
-    const selectedDate = e.target.value;
-
-    // Ottieni il giorno della settimana della data selezionata
-    const selectedDayOfWeek = new Date(selectedDate).getDay();
-
-    // Disabilita la selezione di sabati e domeniche (0 = domenica, 6 = sabato)
-    if (selectedDayOfWeek === 0 || selectedDayOfWeek === 6) {
-      alert("sabato e domenica siamo chiusi");
-    }
-
-    setDataPrenotazione(selectedDate);
-  }
-
-  // Funzione di validazione per disabilitare l'ora dalle 19 alle 8
-  function isDateTimeValid(date) {
-    const selectedDate = new Date(date);
-    const selectedHour = selectedDate.getHours();
-    return selectedHour >= 8 && selectedHour < 19;
-  }
-*/
-  }
   /////////////////
   /////////////////////////////////
   function handleDateTimeChange(e) {
@@ -104,6 +72,24 @@ function Pren() {
 
   const sendReservation = async () => {
     // facciamo una chiamata POST alle api
+    // Controlla se la terapia è già prenotata nella stessa sede e nello stesso orario
+    const isTerapiaOccupata = prenotazioniEsistenti.some(
+      (prenotazione) =>
+        prenotazione.nomeTerapia === nomeTerapia &&
+        prenotazione.sede === sede &&
+        prenotazione.dataPrenotazione === dataPrenotazione
+    );
+
+    if (isTerapiaOccupata) {
+      alert("La terapia selezionata è già prenotata per la data e la sede scelta.");
+      return;
+    }
+
+    // Controlli per assicurarsi che tutti i campi siano stati compilati
+    if (!nomeTerapia || !dataPrenotazione || !sede || !nomeUtente || !cognomeUtente || !emailUtente) {
+      alert("Tutti i campi sono obbligatori. Assicurati di compilare tutti i campi prima di procedere.");
+      return;
+    }
     try {
       let response = await fetch("http://localhost:8080/api/prenotazioni", {
         method: "POST",
@@ -115,10 +101,13 @@ function Pren() {
 
       if (response.ok) {
         alert("PRENOTAZIONE SALVATA CORRETTAMENTE!");
-        // svuotiamo il form resettando l'oggetto state
-        // al suo valore iniziale!
+        setNomeTerapia("");
+        setDataPrenotazione("");
+        setSede("");
+        setNomeUtente("");
+        setCognomeUtente("");
+        setEmailUtente("");
       } else {
-        // errore nei dati inviati? server in crash?
         alert("ERRORE!");
         console.log("errore nella chiamata :(");
       }
@@ -128,12 +117,15 @@ function Pren() {
   };
 
   const [pre, setPre] = useState([]);
+  const [prenotazioniEsistenti, setPrenotazioniEsistenti] = useState([]);
+
   const getAllPren = async () => {
     try {
       let resp = await fetch("http://localhost:8080/api/prenotazioni");
       if (resp.ok) {
         let data = await resp.json();
         setPre(data);
+        setPrenotazioniEsistenti(data);
         console.log("PRENOTAZINIII", data);
       } else {
         console.log("error");
@@ -147,6 +139,7 @@ function Pren() {
     getAllPren();
     generateRandomId();
   }, []);
+
   return (
     <Container>
       <Row className="justify-content-center mt-5">
@@ -159,19 +152,80 @@ function Pren() {
               console.log("i dati sono già pronti nello state:", formData);
               sendReservation();
             }}>
+            <Form.Group className="mb-3">
+              <Form.Label> Numero Prenotazione</Form.Label>
+              <Form.Control type="number" placeholder="Cognome" required value={id} readOnly disabled />
+            </Form.Group>
+
+            {/*  <div>
+              <label>
+                Numero Prenotazione
+                <input type="number" value={id} readOnly disabled />
+                {/* <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+              </label>
+            </div>*/}
+
+            <Form.Group className="mb-3">
+              <Form.Label>Terapia</Form.Label>
+              <Form.Select required value={nomeTerapia} onChange={(e) => setNomeTerapia(e.target.value)}>
+                <option value="">Seleziona una terapia</option>
+                <option>Visita neurologica</option>
+                <option>Visita odontoiatrica</option>
+                <option>Visita otorinolaringoiatrica</option>
+                <option>Visita pneumologica</option>
+                <option>Visita epatologica</option>
+                <option>Visita dermatologica</option>
+                <option> Esame campo visivo</option>
+                <option>MOC</option>
+                <option>Visita senologica</option>
+                <option>Visita cardiologica</option>
+                <option>Visita di chirurgia pancreatica</option>
+                <option>Visita tricologica</option>
+                <option>Visita nefrologica</option>
+                <option>Visita onco-ematologica</option>
+                <option>Visita podologica</option>
+              </Form.Select>
+            </Form.Group>
+            {/* 
             <div>
               <label>
-                Terapie:
-                <input type="number" value={id} readOnly />
-                {/* <input type="text" value={id} onChange={(e) => setId(e.target.value)} />*/}
+                Nome Terapia:
+                <select value={nomeTerapia} onChange={(e) => setNomeTerapia(e.target.value)}>
+                  <option value="">Seleziona una terapia</option>
+                  <option value="visitaneurologica">Visita neurologica</option>
+                  <option value="visitaodontoiatrica">Visita odontoiatrica</option>
+                  <option value="visita otorinolaringoiatrica">Visita otorinolaringoiatrica</option>
+                  <option value="visitapneumologica">Visita pneumologica</option>
+                  <option value="visitaepatologica">Visita epatologica</option>
+                  <option value="visitadermatologica">Visita dermatologica</option>
+                  <option value="esamecampovisivo"> Esame campo visivo</option>
+                  <option value="MOC">MOC</option>
+                  <option value="visitasenologica">Visita senologica</option>
+                  <option value="visitacardiologica">Visita cardiologica</option>
+                  <option value="visitadichirurgiapancreatica">Visita di chirurgia pancreatica</option>
+                  <option value="visitatricologica">Visita tricologica</option>
+                  <option value="visitanefrologica">Visita nefrologica</option>
+                  <option value="visitaoncoematologica">Visita onco-ematologica</option>
+                  <option value="visitapodologica">Visita podologica</option>
+                </select>
               </label>
             </div>
-            <div>
+            */}
+            {/* <div>
               <label>
                 Terapie:
-                <input type="text" value={nomeTerapia} onChange={(e) => setNomeTerapia(e.target.value)} />
+                <select id="terapia" value={nomeTerapia} onChange={(e) => setNomeTerapia(e.target.value)} />
+                <option value="">
+                  Seleziona una terapia
+                  {nomeTerapia.map((terapia) => (
+                    <option key={terapia.id} value={terapia.nome}>
+                      {terapia.nome}
+                    </option>
+                  ))}
+                </option>
               </label>
             </div>
+                  */}
             {/* <DatePicker
               id="dataPrenotazione"
               selected={dataPrenotazione}
@@ -184,7 +238,7 @@ function Pren() {
               dateFormat="yyyy-MM-dd HH:mm" // Formato data e ora
               required
             /> */}
-
+            {/* 
             <div>
               <label>
                 Data Prenotazione:
@@ -197,35 +251,158 @@ function Pren() {
                 />
               </label>
             </div>
+          */}
 
+            <Form.Group className="mb-3">
+              <Form.Label>Data e ora</Form.Label>
+              <Form.Control
+                type="datetime-local"
+                required
+                value={dataPrenotazione}
+                min={getFormattedDateTime(new Date())}
+                max={getFormattedDateTime(new Date(Date.now() + 365 * 24 * 60 * 60 * 1000))} // Imposta il limite massimo a 1 anno da oggi
+                onChange={handleDateTimeChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Sede</Form.Label>
+              <Form.Select required value={sede} onChange={(e) => setSede(e.target.value)}>
+                <option value="">Seleziona una sede</option>
+                <option>Alghero</option>
+                <option>Ancona</option>
+                <option>Aosta</option>
+                <option>Arezzo</option>
+                <option>Avellino</option>
+                <option>Bologna</option>
+                <option>Brescia</option>
+                <option>Catania</option>
+                <option>Cosanza</option>
+                <option>Forlì</option>
+                <option>Frosinone</option>
+                <option>Genova</option>
+                <option>L'Aquila</option>
+                <option>Lecce</option>
+                <option>Mantova</option>
+                <option>Milano</option>
+                <option>Napoli</option>
+                <option>Padova</option>
+                <option>Palermo</option>
+                <option>Perugia</option>
+                <option>Potenza</option>
+                <option>Reggio Calabria</option>
+                <option>Roma</option>
+                <option>Salerno</option>
+                <option>Termoli</option>
+                <option>Torino</option>
+                <option>Trento</option>
+                <option>Udine</option>
+              </Form.Select>
+            </Form.Group>
+            {/* 
             <div>
               <label>
                 Sede:
-                <input type="text" value={sede} onChange={(e) => setSede(e.target.value)} />
+                <select value={sede} onChange={(e) => setSede(e.target.value)}>
+                  <option value="">Seleziona una sede</option>
+                  <option value="alghero">Alghero</option>
+                  <option value="ancona">Ancona</option>
+                  <option value="aosta">Aosta</option>
+                  <option value="arezzo">Arezzo</option>
+                  <option value="avellino">Avellino</option>
+                  <option value="bologna">Bologna</option>
+                  <option value="brescia">Brescia</option>
+                  <option value="catania">Catania</option>
+                  <option value="cosenza">Cosanza</option>
+                  <option value="forli">Forlì</option>
+                  <option value="frosinone">Frosinone</option>
+                  <option value="genova">Genova</option>
+                  <option value="laquila">L'Aquila</option>
+                  <option value="lecce">Lecce</option>
+                  <option value="mantova">Mantova</option>
+                  <option value="milano">Milano</option>
+                  <option value="napoli">Napoli</option>
+                  <option value="padova">Padova</option>
+                  <option value="palermo">Palermo</option>
+                  <option value="perugia">Perugia</option>
+                  <option value="potenza">Potenza</option>
+                  <option value="reggiocalabria">Reggio Calabria</option>
+                  <option value="roma">Roma</option>
+                  <option value="salerno">Salerno</option>
+                  <option value="termoli">Termoli</option>
+                  <option value="torino">Torino</option>
+                  <option value="trento">Trento</option>
+                  <option value="udine">Udine</option>
+                </select>
               </label>
             </div>
-            <div>
+*/}
+            <Form.Group className="mb-3">
+              <Form.Label>Inserisci il tuo nome</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Nome"
+                required
+                value={nomeUtente}
+                onChange={(e) => setNomeUtente(e.target.value)}
+              />
+            </Form.Group>
+            {/*  <div>
               <label>
                 Nome Utente:
                 <input type="text" value={nomeUtente} onChange={(e) => setNomeUtente(e.target.value)} />
               </label>
             </div>
+          
             <div>
               <label>
                 Cognome Utente:
                 <input type="text" value={cognomeUtente} onChange={(e) => setCognomeUtente(e.target.value)} />
               </label>
             </div>
-            <div>
+          */}
+            <Form.Group className="mb-3">
+              <Form.Label>Inserisci il tuo cognome</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Cognome"
+                required
+                value={cognomeUtente}
+                onChange={(e) => setCognomeUtente(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Inserisci la tue Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="email"
+                required
+                value={emailUtente}
+                onChange={(e) => setEmailUtente(e.target.value)}
+              />
+            </Form.Group>
+            {/* <div>
               <label>
                 Email:
                 <input type="email" value={emailUtente} onChange={(e) => setEmailUtente(e.target.value)} />
               </label>
             </div>
-            <Button variant="primary" type="submit">
+            */}
+
+            <Button style={{ backgroundColor: "#2b5453", border: "none" }} type="submit">
               PRENOTA!
             </Button>
           </Form>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Link to="/lemieprenotazioni">
+            <Button
+              style={{ backgroundColor: "#2b5453", border: "none", position: "absolute", top: "135px", right: "12px" }}>
+              Le mie prenotazioni
+            </Button>
+          </Link>
+          {/*   <LeMiePrenotazioni pre={pre} /> */}
         </Col>
       </Row>
     </Container>
