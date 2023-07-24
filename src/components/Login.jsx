dd2db814631293c8b39ede2306e0393b93672b47
Eleonora Mattera <m.eleonora98@live.it>
2023-07-25 00:03:34 +0200

    fix Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,83 @@
-import React, { useState } from "react";
-import { Container, Form, Button } from "react-bootstrap";
-import { useDispatch } from "react-redux";
-import { Link, Navigate } from "react-router-dom";
-/*import { SetTokenType, setToken } from "../redux/actions/UserActions";*/
+import { useEffect, useState } from "react";
+import { Button, Card, Form } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../redux/actions/userAction";
 
 function Login() {
+  /* const admin = {
+    username: "",
+    password: "",
+  }; */
+  const user = {
+    username: "",
+    password: "",
+  };
+
+  const [input, setInput] = useState(user);
   const dispatch = useDispatch();
+
+  const handleChange = (field, value) => {
+    setInput((prev) => ({ ...prev, [field]: value }));
+  };
+
+  return (
+    <>
+      <div className="d-flex justify-content-center">
+        <Card className="text-center w-25 my-5 cardCarrello">
+          <Card.Body>
+            <Card.Title className="mb-4">Login</Card.Title>
+            <Form>
+              <Form.Group md="4" controlId="validationCustom01" className="mb-4">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Username"
+                  onChange={(e) => handleChange("username", e.target.value)}
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group md="4" controlId="validationCustom02" className="mb-4">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  required
+                  type="password"
+                  placeholder="Password"
+                  onChange={(e) => handleChange("password", e.target.value)}
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+            </Form>
+            <Button
+              className="mt-2 mb-4  buttonO"
+              onClick={() => {
+                dispatch(loginUser(input));
+              }}>
+              Submit form
+            </Button>
+            <div>Non sei registrato?</div>
+            <Button href="/registrazione" className="mt-2 mb-4  buttonO">
+              Crea il tuo account!
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+  );
+}
+
+export default Login;
+
+/*import React, { useState } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { SetTokenType, setToken } from "../redux/actions/index";
+
+function Login() {
+   const dispatch = useDispatch();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [logged, setLogged] = useState(false);
+
   const handleLogin = (e) => {
     const url = "http://localhost:8080/api/auth/login";
     var dataObj = {
@@ -32,21 +101,22 @@ function Login() {
       .then((data) => {
         dataObj = data;
 
-        /* dispatch(setToken(dataObj));*/
+        dispatch(setToken(dataObj));
       })
       .catch((error) => {
         console.error(error);
       });
 
     e.preventDefault();
-    // Logica di autenticazione qui...
+
+    const token = dataObj.accessToken;
+    sessionStorage.setItem("token", token);
+    const gettoken = sessionStorage.getItem("token");
+    console.log(typeof gettoken, "TOKEN");
   };
 
-  if (logged) {
-    return <Navigate to="/" />;
-  }
-
   return (
+    
     <Container className="login-container mt-5">
       <h1 className="login-title">Login</h1>
       <Form className="login-form" onSubmit={handleLogin}>
@@ -72,12 +142,14 @@ function Login() {
           />
         </Form.Group>
 
-        <Button variant="primary" type="submit" className="login-button" onClick={console.log("loggato")}>
+        <Button variant="primary" type="submit" className="login-button">
           Login
         </Button>
       </Form>
     </Container>
-  );
-}
-
-export default Login;
+    );
+  }
+  
+  export default Login;
+  
+  */
