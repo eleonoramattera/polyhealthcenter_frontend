72711fe42a2c505bc8a52de6563c8362c56bbabb
Eleonora Mattera <m.eleonora98@live.it>
2023-09-27 23:33:39 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import CustomNav from "./components/CustomNav/CustomNav";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 
 import Login from "./components/Login.jsx";
 import Logout from "./components/Logout";
 import Home from "./components/Home/Home.jsx";
 import ChiSiamo from "./components/ChiSiamo/ChiSiamo.jsx";
 
-import Prenotazioni from "./components/Prenotazioni";
 import TerapiaStore from "./components/TerapiaStore";
 import DoveSiamo from "./components/DoveSiamo";
 import PrenotazioniList from "./components/PrenotazioniList";
 import Registrazione from "./components/Registrazione";
-import LeMiePrenotazioni from "./components/LeMiePrenotazioni";
+
 import Pren from "./components/Pren";
 
 function App() {
@@ -27,15 +25,16 @@ function App() {
           <Route path="/registrazione" element={<Registrazione />} />
           <Route path="/" element={<Home />} />
 
-          <Route path="prenotazioni" element={<Pren />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
+
+          <Route path="prenotazioni" element={<Pren />} />
           <Route path="/chisiamo" element={<ChiSiamo />} />
           <Route path="/terapiastore" element={<TerapiaStore />} />
           <Route path="/dovesiamo" element={<DoveSiamo />} />
           <Route path="/prenotazionilist" element={<PrenotazioniList />} />
         </Routes>
-        {/* <Footer /> */}
+        <Footer />
       </BrowserRouter>
     </div>
   );
